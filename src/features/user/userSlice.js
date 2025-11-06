255415fa306c406ce03dada6f365f48f73bd47e0
Amara-chi <namarachi525@example.com>
2025-11-06 08:42:28 +0100

    debugging backend errors

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = '/api';
 
 // Async thunks for user management
 // In your userSlice.js, update the getUsers thunk:
